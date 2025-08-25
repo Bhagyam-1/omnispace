eb766c4490bbfa407afe33136465b56185d1ded9
Bhagyam-1 <bhagyampandey@gmail.com>
2025-08-25 19:30:46 +0530

    Build issue fix

--- app/connect/create/page.tsx
+++ app/connect/create/page.tsx
@@ -1,7 +1,6 @@
 export const dynamic = "force-dynamic";
 
 import CreateEditPost from "../_components/create-edit-post/create-edit-post";
-import CreateLoader from "./loading";
 
 const CreatePostPage = () => {
     return (
